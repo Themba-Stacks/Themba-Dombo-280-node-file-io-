1091705a75a94774fd1fdd7c40455e63c13eee3b
Themba-Dombo <themba.dombo@gmail.com>
2020-08-13 18:39:25 +0200

    completed load function and added delete function to use with tests

--- src/main.js
+++ src/main.js
@@ -1,17 +1,57 @@
+var fs = require('fs');
+
 class Vistor {
-    constructor(fullName){
+    constructor(fullName, age, dateOfVisit, timeOfVisit, comments, assistedBy){
         this.fullName = fullName;
+        this.age = age;
+        this.dateOfVisit = dateOfVisit;
+        this.timeOfVisit = timeOfVisit;
+        this.comments = comments;
+        this.assistedBy = assistedBy;
     };
 
-    save(){
-        return 'saved'    
+    save(filePath = "../results"){
+        let saveObject = {full_name:this.fullName, age:this.age, date_of_visit:this.dateOfVisit, time_of_visit:this.timeOfVisit, comments:this.comments, assisted_by:this.assistedBy};
+        let saveJSON = JSON.stringify(saveObject);
+
+        let saveName = this.fullName.toLowerCase().replace(' ','_');        
+        fs.writeFile(`${filePath}/visitor_${saveName}.json`, saveJSON, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
     }
 
-    load(nameInput){
-        console.group(nameInput)
+    delete(filePath = "../results"){
+        let deleteName = this.fullName.toLowerCase().replace(' ','_');        
+        fs.unlink(`${filePath}/visitor_${deleteName}.json`,  function (err) {
+            if (err) throw err;
+            let name = deleteName.split('_')
+            console.log(`${name[0]} file deleted!`);
+          });
     }
+
 };
 
+function load (fullNameInput,filePath = "../results") {
+    let personName = fullNameInput.toLowerCase().replace(' ','_');
+    let personObject='';
+    fs.readFile(`${filePath}/visitor_${personName}.json`, (err, data)=>{
+        if (err) throw err;
+        console.log("file read successful");
+        personObject = JSON.parse(data);
+        console.log(personObject);
+    })
+};
 
+var John = new Vistor;
+John.fullName = "John Vader";
+John.age = 21;
+John.dateOfVisit = "2020/03/21";
+John.timeOfVisit = "11:15";
+John.comments = "came to see the manager about the cleaner job";
+John.assistedBy = "Velda"
 
-module.exports = Vistor;
+//John.save();
+// load('John Vader');
+
+module.exports = {Vistor,load};
